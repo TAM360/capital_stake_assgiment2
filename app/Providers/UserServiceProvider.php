8f0a65bf3b7fdf6dd88d444bcdd99e1433c349f0
= <=>
2019-09-22 16:01:59 +0500

    orders table. factory and seeder files modied

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -6,7 +6,11 @@ use Illuminate\Support\ServiceProvider;
 use Yajra\DataTables\DataTables;
 use Silber\Bouncer\BouncerFacade as Bouncer;
 use App\Customer;
+use App\Product;
 use App\User;
+use App\Order;
+use App\OrderItem;
+use phpDocumentor\Reflection\Types\String_;
 
 class UserServiceProvider extends ServiceProvider
 {
@@ -35,15 +39,40 @@ class UserServiceProvider extends ServiceProvider
             ->make(true);
     }
 
-    public function getDataByRole(User $user) {
+    public function getProductData() {
+        $data = Product::latest()->get();
+        return Datatables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action', function($row) {
+                $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+                return $btn;
+            })
+            ->editColumn('in_stock', function ($data) {
+                return ($data->in_stock == 1) ? "Yes": "No";
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    public function getOrderData() {
+        $data = Order::latest()->get();
+        return Datatables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action', function($row) {
+                $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+                return $btn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    public function getDataByRole(User $user, String $type) {
         if (Bouncer::is($user)->a('user-manager') == true) {
             return array($this->getUsersData(), $this->getCustomerData());
-        } else if (Bouncer::is($user)->a('shop-manager') == true) {
-
-        } else if (Bouncer::is($user)->an('admin') == true) {
-
-        } else {
-
+        } else if (Bouncer::is($user)->a('shop-manager') == true && $type == "list_products") {
+            return $this->getProductData();
+        } else if (Bouncer::is($user)->a('shop-manager') == true && $type == "list_orders") {
+            return $this->getOrderData();
         }
     }
 }
--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -12,6 +12,7 @@ $factory->define(Order::class, function (Faker $faker) {
     $num3 = $faker->numberBetween($min = 1000, $max = 9999);
     $randomInvoice = "{$num1}-{$num2}-{$num3}";
     return [
+        'customer_id' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 1, $max = 200),
         'invoice_number' => $randomInvoice,
         'total_amount' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 100, $max = 1000),
         'status' => $status[$faker->numberBetween($min = 0, $max = 1)],
